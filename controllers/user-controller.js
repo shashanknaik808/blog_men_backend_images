c45f5d232b5324dc4e9b685b0846943fed204039
Shashank Naik <shashanknaik808@gmail.com>
2023-12-16 15:40:18 +0530

    Created user controller to retrieve all users from the database

---
+++ controllers/user-controller.js
@@ -0,0 +1,19 @@
+const User = require("../model/User.js");
+
+const getAllUser = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find();
+    }
+    catch (err) {
+        console.log(err);
+    }
+    if (!users) {
+        return res.status(404).json({
+            message: "No Users Found"
+        });
+    }
+    return res.status(200).json({ users });
+}
+
+module.exports = getAllUser;
