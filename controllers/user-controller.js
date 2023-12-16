74af0d70bbf851ff372faf298f03a2d7eb015c9d
Shashank Naik <shashanknaik808@gmail.com>
2023-12-16 16:21:08 +0530

    Implemented login controller with password verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -48,3 +48,27 @@ const signup = async (req, res, next) => {
     return res.status(201).json({ user });
 };
 module.exports = signup;
+
+
+// Login
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email });
+    } catch (err) {
+        return console.log(err);
+    }
+    if (!existingUser) {
+        return res.status(404).json({ message: "Couldnt Find User By This Email" });
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Incorrect Password" });
+    }
+    return res
+        .status(200)
+        .json({ message: "Login Successfull", user: existingUser });
+};
+module.exports = login;
