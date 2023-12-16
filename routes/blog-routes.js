32dce54e006040452080d83cd8c091b0db29fcfe
Shashank Naik <shashanknaik808@gmail.com>
2023-12-16 16:52:26 +0530

    Configured route to handle POST requests for adding new blog entries

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const getAllBlogs = require("../controllers/blog-controller.js");
+const addBlog = require("../controllers/blog-controller.js");
 
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
+blogRouter.post("/add", addBlog);
 
 module.exports = blogRouter;
