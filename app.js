e229583734476b2a58faf869e88ca85371f59a84
Shashank Naik <shashanknaik808@gmail.com>
2023-12-16 16:48:10 +0530

    Integrated blog routes in app.js and added getAllBlogs controller for fetching all blogs

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require("./routes/user-routes.js");
-
+const blogRouter = require("./routes/blog-routes.js");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
 
 mongoose
     .connect(
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const getAllBlogs = require("../controllers/blog-controller.js");
 
 const blogRouter = express.Router();
+
+blogRouter.get("/", getAllBlogs);
 
 module.exports = blogRouter;
