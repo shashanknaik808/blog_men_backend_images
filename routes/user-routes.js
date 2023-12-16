194986737601c3d201bbaee21177796ba6b849f6
Shashank Naik <shashanknaik808@gmail.com>
2023-12-16 16:22:42 +0530

    Added route for user login

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const getAllUser = require("../controllers/user-controller.js");
 const signup = require("../controllers/user-controller.js");
+const login = require("../controllers/user-controller.js");
 
 const router = express.Router();
 
 router.get("/", getAllUser);
 router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
