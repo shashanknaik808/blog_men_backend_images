d0a38494c181fe50fd2a2bbd95645c0be45437e1
Shashank Naik <shashanknaik808@gmail.com>
2023-12-18 11:22:50 +0530

    Implemented endpoint and controller for deleting a blog entry

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -56,3 +56,21 @@ const updateBlog = async (req, res, next) => {
     return res.status(200).json({ blog });
 };
 module.exports = updateBlog;
+
+//deleteBlog
+const deleteBlog = async (req, res, next) => {
+    const id = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndRemove(id).populate("user");
+        await blog.user.blogs.pull(blog);
+        await blog.user.save();
+    } catch (err) {
+        console.log(err);
+    }
+    if (!blog) {
+        return res.status(500).json({ message: "Unable To Delete" });
+    }
+    return res.status(200).json({ message: "Successfully Delete" });
+}
+module.exports = deleteBlog;
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const getAllBlogs = require("../controllers/blog-controller.js");
 const addBlog = require("../controllers/blog-controller.js");
 const updateBlog = require("../controllers/blog-controller.js");
+const deleteBlog = require("../controllers/blog-controller.js");
 
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
 blogRouter.put("/update/:id", updateBlog);
+blogRouter.delete("/:id", deleteBlog);
 
 module.exports = blogRouter;
