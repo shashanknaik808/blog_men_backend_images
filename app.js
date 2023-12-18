14d9599db98d20adc7d0c4154cefff460734fb9e
Shashank Naik <shashanknaik808@gmail.com>
2023-12-18 15:47:55 +0530

    Uploading images to backend as file(MongoDB Atlas)

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/api/blog", blogRouter);
 
 mongoose
     .connect(
-        "mongodb+srv://shashanknaik808:qwerty123456@cluster0.rz6q9l5.mongodb.net/?retryWrites=true&w=majority"
+        "mongodb+srv://shashanknaik808:qwerty123456@cluster0.rz6q9l5.mongodb.net/UserBlog?retryWrites=true&w=majority"
     )
     .then(() => app.listen(5000))
     .then(() =>
