662f2d277b714b597cfdf7fc9d97999a06ad62a3
Shashank Naik <shashanknaik808@gmail.com>
2023-12-18 12:12:01 +0530

    Checking request object

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -19,59 +19,64 @@ module.exports.getAllBlogs = async (req, res, next) => {
 
 //addBlog
 module.exports.addBlog = async (req, res, next) => {
-    const { title, description, image, user } = req.body;
+    console.log(req.body);
+    console.log(req.files);
 
-    let existingUser;
-    try {
-        existingUser = await User.findById(user);
-    } catch (err) {
-        return console.log(err);
-    }
-    if (!existingUser) {
-        return res.status(400).json({ message: "Unable TO Find User By This ID" });
-    }
-    const blog = new Blog({
-        title,
-        description,
-        image,
-        user,
-    });
-    try {
-        const session = await mongoose.startSession();
-        session.startTransaction({ session });
-        await blog.save({ session });
-        existingUser.blogs.push(blog);
-        await existingUser.save({ session });
-        await session.commitTransaction();
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: err });
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ msg: 'No files were uploaded, please check' });
     }
 
-    return res.status(200).json({ blog });
+    // let existingUser;
+    // try {
+    //     existingUser = await User.findById(user);
+    // } catch (err) {
+    //     return console.log(err);
+    // }
+    // if (!existingUser) {
+    //     return res.status(400).json({ message: "Unable TO Find User By This ID" });
+    // }
+    // const blog = new Blog({
+    //     title,
+    //     description,
+    //     image,
+    //     user,
+    // });
+    // try {
+    //     const session = await mongoose.startSession();
+    //     session.startTransaction({ session });
+    //     await blog.save({ session });
+    //     existingUser.blogs.push(blog);
+    //     await existingUser.save({ session });
+    //     await session.commitTransaction();
+    // } catch (err) {
+    //     console.log(err);
+    //     return res.status(500).json({ message: err });
+    // }
+
+    return res.status(200).json({ status: 'success' });
 };
 
 
 //updateBlog
 module.exports.updateBlog = async (req, res, next) => {
-    const { title, description, image } = req.body;
-    const blogId = req.params.id;
-    let blog;
-    try {
-        await Blog.updateOne({ "_id": blogId }, {
-            title: title,
-            description: description,
-            image: image,
-        });
-        blog = await Blog.findById(blogId);
-    } catch (err) {
-        return console.log(err);
-    }
-    if (!blog) {
-        return res.status(500).json({ message: "Unable To Update The Blog" });
-    }
+    // const { title, description, image } = req.body;
+    // const blogId = req.params.id;
+    // let blog;
+    // try {
+    //     await Blog.updateOne({ "_id": blogId }, {
+    //         title: title,
+    //         description: description,
+    //         image: image,
+    //     });
+    //     blog = await Blog.findById(blogId);
+    // } catch (err) {
+    //     return console.log(err);
+    // }
+    // if (!blog) {
+    //     return res.status(500).json({ message: "Unable To Update The Blog" });
+    // }
 
-    return res.status(200).json({ blog });
+    // return res.status(200).json({ blog });
 };
 
 
