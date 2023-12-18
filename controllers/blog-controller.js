ea91ac976d121eec9a46b70797af992db5ceab4a
Shashank Naik <shashanknaik808@gmail.com>
2023-12-18 11:17:05 +0530

    Implemented endpoint and controller for updating a blog entry

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -36,3 +36,23 @@ const addBlog = async (req, res, next) => {
     return res.status(200).json({ blog });
 }
 module.exports = addBlog;
+
+//updateBlog
+const updateBlog = async (req, res, next) => {
+    const { title, description } = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description,
+        });
+    } catch (err) {
+        return console.log(err);
+    }
+    if (!blog) {
+        return res.status(500).json({ message: "Unable To Update The Blog" });
+    }
+    return res.status(200).json({ blog });
+};
+module.exports = updateBlog;
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const getAllBlogs = require("../controllers/blog-controller.js");
 const addBlog = require("../controllers/blog-controller.js");
+const updateBlog = require("../controllers/blog-controller.js");
 
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
+blogRouter.put("/update/:id", updateBlog);
 
 module.exports = blogRouter;
