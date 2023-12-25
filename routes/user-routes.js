144788a66fe73a00d3b5f12639511c8283755c2a
Shashank Naik <shashanknaik808@gmail.com>
2023-12-25 16:34:48 +0530

    Format and add route for user retrieval in userRouter

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,13 +2,16 @@ const express = require('express');
 const {
     getAllUser,
     signup,
-    login
+    login,
+    retrive
 } = require("../controllers/user-controller.js");
+
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUser);
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
+userRouter.get('/:id', retrive)
 
 module.exports = userRouter;
