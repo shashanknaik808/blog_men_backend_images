b6c53deca65914b9b7f67fbb681c1add74e1f096
Shashank Naik <shashanknaik808@gmail.com>
2023-12-25 16:37:26 +0530

    Format and reorganize server setup in app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRouter = require("./routes/user-routes.js");
-const blogRouter = require("./routes/blog-routes.js");
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
 
+const blogRouter = require("./routes/blog-routes.js");
+const userRouter = require("./routes/user-routes.js");
+
+const cors = require('cors');
+const fileupload = require('express-fileupload');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(fileUpload());
-app.use('/photo', express.static('upload'));
+app.use(fileupload());
 
+app.use('/photo', express.static('upload'));
 app.use("/api/user", userRouter);
 app.use("/api/blog", blogRouter);
 
@@ -22,4 +24,4 @@ mongoose
     .then(() =>
         console.log("Connected TO Database and Listening TO Localhost 5000")
     )
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err)); 
